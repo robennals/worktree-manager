3a7912ffa6f20a5093195c2c3c6d0a76806e12ee
Rob Ennals <rob@ennals.org>
2026-01-12 12:32:03 -0800

    Don't include dist in repo. Gitignore it.

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./commands/index.js";
-export * from "./utils/git.js";
-export * from "./utils/env.js";
-export * from "./utils/github.js";
-export * from "./utils/editor.js";
-//# sourceMappingURL=index.d.ts.map
